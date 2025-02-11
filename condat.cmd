1b0fde9bc8adf8f8ba654f330f0382d0d36921c9
Yan-Xiangjun <xmascreater@163.com>
2025-02-12 00:00:54 +0800

    更新启动脚本

--- condat.cmd
+++ condat.cmd
@@ -1,5 +1,5 @@
 if not exist "%~dp0condat_venv\" (
     python -m venv "%~dp0condat_venv"
-    %~dp0condat_venv\Scripts\pip.exe install textual==1.0.0
+    "%~dp0condat_venv\Scripts\pip.exe" install textual==1.0.0
 )
-%~dp0condat_venv\Scripts\python.exe %~dp0main.py
+"%~dp0condat_venv\Scripts\python.exe" "%~dp0main.py"
